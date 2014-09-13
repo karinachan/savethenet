ed9fd3c5ce0684c5aaf96d171beb59bde4fa85b0
Claire S <Claire@seas.2047.apng.wireless-pennnet.upenn.edu>
2014-09-13 01:49:40 -0400

    Bootstrap navbar

--- profile.php
+++ profile.php
@@ -7,6 +7,22 @@
 </head>
 <body>
 <div id="container">
+	<nav class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navnav">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navnav">
+          <button type="button" class="btn btn-default navbar-btn">Log Out</button>
+        </div>
+      </div>
+    </nav>
 	<div id="left-panel">
 
 	</div>
