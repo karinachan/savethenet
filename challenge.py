30ac75b838bd0d59640e218878a04fc348e65c64
Claire S <Claire@seas.2047.apng.wireless-pennnet.upenn.edu>
2014-09-13 12:53:15 -0400

    Adding Social Media Integration

---
+++ challenge.py
@@ -0,0 +1,7 @@
+class Challenge: 
+	def __init__(self, title, complete, pts, modalID, pss):
+		self.title = title
+		self.completed = complete
+		self.points = pts
+		modalID.txt = modalID 
+		self.passed = pss  
---
+++ user.py
@@ -0,0 +1,5 @@
+class User: 
+	def __init__(self, usr_id, pts, chall):
+		self.user_id = usr_id
+		self.points = pts
+		self.challenge_list = chall
