5c63c7967d38893b800bc593bc6c6fd261138f04
Karina <kchan4@wellesley.edu>
2014-09-13 04:11:39 -0400

    pymongo!

--- connection.py
+++ connection.py
@@ -1,16 +1,25 @@
 import pymongo
+import datetime
+from pymongo import MongoClient
+
 
 # Connection to Mongo DB
-try:
-    conn=pymongo.MongoClient('ds035240.mongolab.com:35240/savethedata', 37000)
-    db = conn["savethedata"]
-    db.authenticate("admin", "catmin")
 
+post = {"author": "Karina",
+         "text": "Pymongo insertion #1!",
+         "tags": ["pymongo", "test", "number 1"],
+         "date": datetime.datetime.utcnow()}
+
+
+try:
+    client = MongoClient('mongodb://admin:catmin@ds035240.mongolab.com:35240/savethedata')
     print "Connected successfully!!!"
-    widgets = db.widgets
-    json_data = open("widget.json")
-    new_widget = json.load(json_data)
-    widgets.insert(new_widget)
+    db=client['savethedata']
+    collection=db['savethedata1']
+    print "collection established"
+    posts=db.posts
+    post_id=posts.insert(post)
+    print post_id
+
 except pymongo.errors.ConnectionFailure, e:
    print "Could not connect to MongoDB: %s" % e
-conn
