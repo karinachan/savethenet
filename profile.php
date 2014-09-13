8aafe2bbcdf298c40868436b75cde66bb40b09f9
Claire S <Claire@seas.2047.apng.wireless-pennnet.upenn.edu>
2014-09-13 02:21:56 -0400

    Bootstrap nav

--- profile.php
+++ profile.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 <div id="container">
+
+	<!-- NAVBAR --> 
 	<nav class="navbar navbar-default" role="navigation">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -16,22 +18,40 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#">Save the Net</a>
         </div>
         <div class="collapse navbar-collapse" id="navnav">
           <button type="button" class="btn btn-default navbar-btn">Log Out</button>
         </div>
       </div>
     </nav>
+
+    <!-- END NAVBAR --> 
+
+    <!-- ALERT -->
+    <div class="alert alert-danger fade in" role="alert">
+      <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+      <h4>Don't Miss Your Chance to Take Action</h4>
+      <p>The FCC closes comments on the proposed rules on Monday, September 15th!</p>
+      <button type="button" class="btn btn-danger" id="send-comment" onclick='location.href="http://act.freepress.net/sign/internet_fcc_nprm_oliver?source=takeaction"'>Send a Comment Now!</button>
+      </p>
+    </div>
+    <!-- END ALERT -->
+
 	<div id="left-panel">
+		<h3>Your Profile</h3>
 
 	</div>
 	<div id="center-panel">
+		<h3>Take Action</h3>
 
 	</div>
 	<div id="right-panel">
+		<h3>Learn More</h3>
 
 	</div>
 </div>
+
+
 </body>
 </html>
